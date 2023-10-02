10862c1bd68292d60b8fba10818ad0fc725e0d80
amarilis <amarilispequeno@gmail.com>
2023-10-02 19:54:30 -0300

    add model product e product type em constants

--- src/models/product.js
+++ src/models/product.js
@@ -1,12 +1,90 @@
-const {dbConnection} = require('../database/dbConnection');
+import tipoProduto from "../constants/tipoProduto";
+const { INTEGER, BLOB, NUMBER, DATE, STRING } = require("sequelize");
+const { dbConnection } = require("../database/dbConnection");
 
-const Product = dbConnection.define('product', {
+const Product = dbConnection.define(
+  "product",
+  {
+    id: {
+      type: INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
 
+    productName: {
+      type: STRING,
+      allowNull: false,
+    },
 
+    laboratoryName: {
+      type: STRING,
+      allowNull: false,
+    },
 
+    image: {
+      type: BLOB,
+      allowNull: false,
+    },
 
+    dosage: {
+      type: ENUM("mg", "mcg", "g", "mL", "%", "outro"),
+      allowNull: false,
+    },
 
-}
+    productDescription: {
+      type: STRING,
+      allowNull: true,
+    },
+
+    productUnitPrice: {
+      type: NUMBER,
+      allowNull: false,
+    },
+
+    productType: {
+      type: tipoProduto,
+      allowNull: false,
+    },
+
+    quantityStock: {
+      type: INTEGER,
+      allowNull: false,
+    },
+
+    productRegistrationDate: {
+      type: DATE,
+      allowNull: false,
+    },
+
+    idUsers: {
+      type: INTEGER,
+      allowNull: false,
+      references: {
+        model: { tableName: "users" },
+        key: "id",
+      },
+    },
+
+    createdAt: {
+      type: DATE,
+      allowNull: true,
+    },
+
+    updatedAt: {
+      type: DATE,
+      allowNull: true,
+    },
+
+    deletedAt: {
+      type: DATE,
+      allowNull: true,
+    },
+  },
+  {
+    freezeTableName: true,
+    paranoid: true,
+    timestamps: true,
+  }
 );
 
-module.exports = {Product};
+module.exports = { Product };
