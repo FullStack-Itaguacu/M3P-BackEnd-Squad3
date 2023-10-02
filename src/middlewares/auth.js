cedfdbe69516b8e07f9eea83eda7373d29a031af
fhoinaski <sukinodoncai@gmail.com>
2023-10-02 19:43:38 -0300

    adicionado autenticação usando o passport

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,17 +1,16 @@
-const { verify } = require("jsonwebtoken")
-const config = require("../config/config.server") 
-const { HTTP_STATUS } = require("../constants/httpStatus")
-const ERROR_MESSAGES = require("../constants/errorMessages")
+const passport = require('./passport.config');
+const {HTTP_STATUS} = require('../constants/httpStatus');
+const {ERROR_MESSAGES} = require('../constants/errorMessages');
 
+module.exports = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (error, user) => {
+    if (error || !user) {
 
-async function auth(request, response, next) {
-  try {
-    const { authorization } = request.headers
-    request["payload"] = verify(authorization, config.jwtSecret)
-    next()
-  } catch (error) {
-    return response.status(HTTP_STATUS.UNAUTHORIZED).send(ERROR_MESSAGES.UNAUTHORIZED)
-  }
+      return res.status(HTTP_STATUS.UNAUTHORIZED).json(ERROR_MESSAGES.UNAUTHORIZED);
+    }
+
+    req.user = user;
+
+    next();
+  })(req, res, next);
 }
-
-module.exports = { auth }
