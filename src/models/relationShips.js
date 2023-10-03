c43b32ad64f9f1ebf0e2346bda89c72ba6e376d3
fhoinaski <sukinodoncai@gmail.com>
2023-10-02 23:01:53 -0300

    correção do relacionamento

--- src/models/relationShips.js
+++ src/models/relationShips.js
@@ -1,15 +1,58 @@
+
 const {User} = require('./user');
 const {Address} = require('./address');
 const UserAddress = require('./userAddress');
 
-User.hasMany(UserAddress);
-Address.hasMany(UserAddress);
+const relations = {
 
-UserAddress.belongsTo(User);
-UserAddress.belongsTo(Address); 
+  User: {
+    hasMany: {
+      addresses: {
+        model: UserAddress,
+        as: 'Addresses',
+        foreignKey: 'userId'
+      }
+    },
+    
+    belongsTo: {
+      parent: {
+        model: User,
+        as: 'Parent',
+        foreignKey: 'parentId'
+      },
+      createdBy: {
+        model: User,
+        as: 'CreatedBy',
+        foreignKey: 'created_by'  
+      }
+    }
+  },
+
+  Address: {
+    hasMany: {
+      users: {
+        model: UserAddress,
+        as: 'Users',
+        foreignKey: 'addressId'
+      }
+    }
+  },
+
+  UserAddress: {
+    belongsTo: {
+      user: {
+        model: User, 
+        foreignKey: 'userId'
+      },
+      address: {
+        model: Address,
+        foreignKey: 'addressId'
+      }
+    }
+  }
+
+};
 
 module.exports = {
-  User,
-  Address,
-  UserAddress
+  relations
 };
