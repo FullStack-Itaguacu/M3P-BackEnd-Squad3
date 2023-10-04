b447490ccf6784510eee347c09ef36e0eff21d6c
amarilis <amarilispequeno@gmail.com>
2023-10-04 13:26:55 -0300

    atualização user model e address lat long cep zip

--- src/models/address.js
+++ src/models/address.js
@@ -1,8 +1,6 @@
-
 const { dbConnection } = require("../database/dbConnection");
 const { STRING, DATE, INTEGER, NUMBER } = require("sequelize");
 const StatesEnum = require("../constants/statesEnum");
-
 
 const Address = dbConnection.define(
   "address",
@@ -29,7 +27,7 @@ const Address = dbConnection.define(
       allowNull: false,
     },
 
-    number: {
+    numberStreet: {
       type: STRING,
       allowNull: false,
     },
@@ -54,11 +52,11 @@ const Address = dbConnection.define(
       allowNull: true,
     },
 
-    latitude: {
+    lat: {
       type: STRING,
       allowNull: true,
     },
-    longitude: {
+    long: {
       type: STRING,
       allowNull: true,
     },
