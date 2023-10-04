568f64c5c514cbcc90f0e27b61028e2c296a9a07
fhoinaski <sukinodoncai@gmail.com>
2023-10-04 12:46:21 -0300

    criado o model de relacionamento user address

---
+++ src/models/user_address.js
@@ -0,0 +1,44 @@
+const { DataTypes, Model } = require('sequelize');
+
+const dbConnection = require('../database/dbConnection').dbConnection;
+
+const {User} = require('./user'); 
+const {Address} = require('./address');
+
+class UserAddress extends Model {}
+
+UserAddress.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User, 
+      key: 'id'
+    }
+  },
+  addressId: {
+   type: DataTypes.INTEGER,
+    references: {
+     model: Address,  
+      key: 'id'
+    }
+  }
+}, 
+{
+  freezeTableName: true,
+  sequelize: dbConnection,
+  modelName: 'users_address' 
+});
+
+// Relacionamentos
+User.belongsToMany(Address, { through: UserAddress });
+Address.belongsToMany(User, { through: UserAddress });
+
+UserAddress.belongsTo(User);
+UserAddress.belongsTo(Address);
+
+module.exports = {UserAddress};
