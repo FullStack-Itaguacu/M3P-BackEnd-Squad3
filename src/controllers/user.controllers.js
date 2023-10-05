6ba523a5190feebe4036cfeeff874cdb0a195d82
fhoinaski <sukinodoncai@gmail.com>
2023-10-05 00:00:02 -0300

    adicionado rota de cadastro ususario

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,38 +1,66 @@
-const {User} = require('../models/user');
+const { User } = require("../models/user");
+const { Address } = require("../models/address");
+const { UserAddress } = require("../models/user_address");
+const { HTTP_STATUS } = require("../constants/httpStatus");
+const ERROR_MESSAGES = require("../constants/errorMessages");
+const passwordHasher = require("../utils/passwordHasher");
+const validateUserInput = require("../services/validateUserInput.services");
+const checkEmailOrCPFExists = require("../services/checkEmailOrCPFExists");
 
-class UserController{
+class UserController {
+  createUser = async (req, res) => {
+    const { user, address } = req.body;
+    const {
+      zip,
+      street,
+      numberStreet,
+      neighborhood,
+      city,
+      state,
+      complement,
+      lat,
+      long,
+    } = address;
+    const { fullName, email, cpf, phone, password, birthDate, typeUser } = user;
 
-
-    createUser = (req, res) => {
-        
-        const {name, cpf, dt_birth, email, telephone, password, address_id, type_user} = req.body;
-        User.create({
-            name,
-            cpf,
-            dt_birth,
-            email,
-            telephone,
-            password,
-            address_id,
-            type_user
-        }).then((user) => {
-            res.status(200).json(user);
-        }).catch((error) => {
-            res.status(500).json(error);
-        });
-        
-        
-    }
-
-    //me de um json de cadastro teste.
- 
-
-  
-
-
-
+    const passwordHash = await passwordHasher.hashPassword(password);
     
+    const dataVarify = await validateUserInput({ cpf, email, phone });
 
+    try {
+      if (!dataVarify.isValid) {
+        return res.status(HTTP_STATUS.BAD_REQUEST).send(dataVarify.errors);
+      }
+      const checkEmailCpf = await checkEmailOrCPFExists(email, cpf);
+      if (!checkEmailCpf.isValid) {
+        return res.status(HTTP_STATUS.BAD_REQUEST).send(checkEmailCpf.data);
+      }
+
+      const userCreated = await User.create({
+        fullName,
+        email,
+        cpf: dataVarify.data.cpf,
+        phone,
+        password: passwordHash,
+        birthDate,
+        typeUser,
+      });
+
+      const addressCreated = await Address.create(address);
+
+      const userAddressCreated = await UserAddress.create({
+        userId: userCreated.id,
+        addressId: addressCreated.id,
+      });
+
+      return res.status(HTTP_STATUS.OK).send(userAddressCreated);
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+        .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+    }
+  };
 }
 
 const userController = new UserController();
--- src/routes/v1Routes/user.routes.js
+++ src/routes/v1Routes/user.routes.js
@@ -1,10 +1,13 @@
 const {Router} = require("express")
 const userController = require("../../controllers/user.controllers")
+const userSignupValidatorMiddleware = require("../../middlewares/userSignupValidatorMiddleware ")
+
+
 
 const router = Router()
 
 
-router.post("/user/create", userController.createUser)
+router.post("/user/signup",userSignupValidatorMiddleware, userController.createUser)
 
 
 module.exports = router
