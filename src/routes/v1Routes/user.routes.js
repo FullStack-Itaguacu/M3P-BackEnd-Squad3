3af2afd9aad92193ff165944a36d807e92195135
fhoinaski <sukinodoncai@gmail.com>
2023-10-06 00:03:44 -0300

    correção do auth

--- src/routes/v1Routes/user.routes.js
+++ src/routes/v1Routes/user.routes.js
@@ -2,7 +2,7 @@ const {Router} = require("express")
 const userController = require("../../controllers/user.controllers")
 const userSignupValidatorMiddleware = require("../../middlewares/userSignupValidatorMiddleware ")
 const authLoginMiddleware = require("../../middlewares/authLoginMiddleware")
-const auth = require("../../middlewares/auth")
+
 
 
 
@@ -12,7 +12,7 @@ const router = Router()
 
 router.post("/user/signup",userSignupValidatorMiddleware, userController.createUser)
 router.post("/user/login",authLoginMiddleware, userController.loginUser)
-router.post("/admin/login",auth,authLoginMiddleware, userController.loginUser)
+router.post("/admin/login",authLoginMiddleware, userController.loginUser)
 
 
 module.exports = router
