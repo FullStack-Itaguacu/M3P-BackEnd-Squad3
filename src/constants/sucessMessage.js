4cac84061bdbd18ee0261b509a1c413e214e7e60
amarilis <amarilispequeno@gmail.com>
2023-10-08 17:45:01 -0300

    atualização da rota listar produtos

--- src/constants/sucessMessage.js
+++ src/constants/sucessMessage.js
@@ -1,16 +1,17 @@
 const SUCESS_MESSAGE = {
-    LOGIN: 'Login realizado com sucesso!',
-    LOGOUT: 'Logout realizado com sucesso!',
-    USER_CREATED: {
-        message: 'Usuário criado com sucesso!',
-        code: 'USER_CREATED',
-    },
-    UPDATED: 'Atualização realizada com sucesso!',
-    DELETED: 'Remoção realizada com sucesso!',
-    PASSWORD_CHANGED: 'Senha alterada com sucesso!',
-    PASSWORD_RESET: 'Senha resetada com sucesso!',
+  LOGIN: "Login realizado com sucesso!",
+  LOGOUT: "Logout realizado com sucesso!",
+  USER_CREATED: {
+    message: "Usuário criado com sucesso!",
+    code: "USER_CREATED",
+  },
+  UPDATED: "Atualização realizada com sucesso!",
+  DELETED: "Remoção realizada com sucesso!",
+  PASSWORD_CHANGED: "Senha alterada com sucesso!",
+  PASSWORD_RESET: "Senha resetada com sucesso!",
+  NOT_DATA: "Nenhum dado encontrado",
 };
 
 module.exports = {
-    SUCESS_MESSAGE
+  SUCESS_MESSAGE,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,10 @@
-const { Router } = require("express")
-const userRoutes = require("./v1Routes/user.routes")
-const saleRoutes = require("./v1Routes/sale.routes")
+const { Router } = require("express");
+const userRoutes = require("./v1Routes/user.routes");
+const saleRoutes = require("./v1Routes/sale.routes");
+const productRoutes = require("./v1Routes/product.routes");
 
-const routes = new Router()
+const routes = new Router();
 
-routes.use("/api", [
-    userRoutes,
-    saleRoutes
-    
-])
+routes.use("/api", [userRoutes, saleRoutes, productRoutes]);
 
-module.exports = routes
+module.exports = routes;
