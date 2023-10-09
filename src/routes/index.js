af7959246226ff5abb69fca544f0b2339e3bf584
ThiagoThe <nitrox2thi@gmail.com>
2023-10-08 22:33:13 -0300

    feat: criado admin controller e admin routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,18 @@
-
-
 const { Router } = require("express");
 const userRoutes = require("./v1Routes/user.routes");
 const buyerRoutes = require("./v1Routes/buyer.routes");
-const saleRoutes = require("./v1Routes/sale.routes")
-const productRoutes = require("./v1Routes/product.routes")
+const saleRoutes = require("./v1Routes/sale.routes");
+const productRoutes = require("./v1Routes/product.routes");
+const adminRoutes = require("./v1Routes/admin.routes");
 
 const routes = new Router();
 
 routes.use("/api", [
-    userRoutes,
-    saleRoutes,
-    productRoutes,
-    buyerRoutes,
-    
-])
+  userRoutes,
+  saleRoutes,
+  productRoutes,
+  buyerRoutes,
+  adminRoutes,
+]);
 
 module.exports = routes;
-
--- src/routes/v1Routes/admin.routes.js
+++ src/routes/v1Routes/admin.routes.js
@@ -1,2 +1,16 @@
-const {Router} = require("express")
-const adminController = require("../../controllers/admin.controllers")
+const { Router } = require("express");
+const adminController = require("../../controllers/admin.controllers");
+const userSignupValidatorMiddleware = require("../../middlewares/userSignupValidatorMiddleware ");
+const authLoginMiddleware = require("../../middlewares/authLoginMiddleware");
+const acessControl = require("../../middlewares/accessControlMiddleware");
+
+const router = Router();
+
+router.post(
+  "/user/admin/signup",
+  userSignupValidatorMiddleware,
+  adminController.createUserAdmin
+);
+router.post("/user/admin/login", adminController.loginUser);
+
+module.exports = router;
