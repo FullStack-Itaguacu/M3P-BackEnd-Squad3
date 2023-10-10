3d280a1f257324291000992f52cf4030f0ce3ee1
Patrícia Vasques <patriciahhhh@gmail.com>
2023-10-09 21:54:58 -0300

    fix: coloquei o middlewre de validação na rota post

--- src/routes/v1Routes/sale.routes.js
+++ src/routes/v1Routes/sale.routes.js
@@ -2,12 +2,14 @@ const {Router} = require("express")
 const saleController = require("../../controllers/sale.controllers")
 const acessControl = require("../../middlewares/accessControlMiddleware")
 const typeUserEnum = require("../../constants/enums/typeUserEnum")
+const validatorSales = require("../../middlewares/validatorSalesMiddleware")
 const  auth  = require('../../middlewares/auth')
+
 
 
     const saleRoutes = Router()
 
-    saleRoutes.post('/sales', saleController.createSale)
+    saleRoutes.post('/sales', auth, validatorSales, saleController.createSale)
     saleRoutes.get('/sales', auth, acessControl(typeUserEnum.BUYER), saleController.listSales)
     saleRoutes.get('/sales/admin', auth, acessControl(typeUserEnum.ADMIN), saleController.listSaleAdmin)
 
