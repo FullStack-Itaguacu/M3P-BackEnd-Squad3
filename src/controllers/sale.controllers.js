d06fc4f2c130b1d6c380739730898f44ed575f9e
fhoinaski <sukinodoncai@gmail.com>
2023-10-09 22:59:07 -0300

    adiciado o create sales

--- src/controllers/sale.controllers.js
+++ src/controllers/sale.controllers.js
@@ -7,45 +7,52 @@ const { SUCESS_MESSAGE } = require("../constants/sucessMessage")
 class SaleController {
   createSale = async (req, res) => {
     const createSales = [];
-  
-    try {
-      for (const sale of req.body) {
-        const product = req.product;
-        const buyerId = req.user.id;
-        const sellerId = product.userId;
-  
-        const updateAmountBuyProduct = product.totalStock - sale.amountBuy;
-        await Product.update(
-          { totalStock: updateAmountBuyProduct },
-          { where: { id: sale.productId } }
-        );
-  
-        const unitPrice = product.unitPrice;
-        const total = unitPrice * sale.amountBuy;
-  
-        const newSale = await Sale.create({
-          buyerId,
-          sellerId,
-          productId: sale.productId,
-          amountBuy: sale.amountBuy,
-          userAddressId: sale.userAddressId,
-          total,
-          typePayment: sale.typePayment,
-        });
-  
-        createSales.push(newSale);
-      }
-  
-      console.log(createSales);
+
+  try {
+
+    const buyerId = req.user.id;
+    const products = req.product;
+     
+    
+    
+    for(const sale of req.body) {
       
-      // Enviar a resposta após o loop ter processado todas as vendas
-      return res.status(HTTP_STATUS.CREATED).json(createSales);
-    } catch (error) {
-      console.error(error);
-      return res
-        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
-        .send(ERROR_MESSAGES.FAILED_TO_CREATE);
+
+      const product = products.find(p => p.id === sale.productId);
+      
+      
+      const updateAmount = product.totalStock - sale.amountBuy;
+      
+      await product.update({
+        totalStock: updateAmount  
+      });
+      
+      const unitPrice = product.unitPrice;
+      const total = unitPrice * sale.amountBuy;
+      
+      const newSale = {
+        buyerId,
+        sellerId: product.userId, 
+        productId: sale.productId,
+        amountBuy: sale.amountBuy, 
+        userAddressId: sale.userAddressId,
+        total,
+        typePayment: sale.typePayment
+      };
+      
+      createSales.push(newSale);
     }
+
+
+  await Sale.bulkCreate(createSales);
+ 
+
+    return res.status(HTTP_STATUS.CREATED).json(createSales);
+
+  } catch (error) {
+    console.error(error);
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).send(ERROR_MESSAGES.SERVER_ERROR); 
+  }
   };
 
   listSales = async (req, res) => {
