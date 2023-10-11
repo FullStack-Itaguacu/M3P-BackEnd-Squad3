79ed7903677c10fc71ebe7705b7e69a734e84d07
fhoinaski <sukinodoncai@gmail.com>
2023-10-10 21:58:15 -0300

    fix : adicionado middleware de validação na rota

--- src/routes/v1Routes/admin.routes.js
+++ src/routes/v1Routes/admin.routes.js
@@ -1,16 +1,18 @@
 const { Router } = require("express");
 const adminController = require("../../controllers/admin.controllers");
 const userSignupValidatorMiddleware = require("../../middlewares/userSignupValidatorMiddleware ");
-const authLoginMiddleware = require("../../middlewares/authLoginMiddleware");
 const acessControl = require("../../middlewares/accessControlMiddleware");
+const auth = require("../../middlewares/auth");
+const typeUserEnum = require("../../constants/enums/typeUserEnum");
+const authMiddleware = require("../../middlewares/authLoginMiddleware");
 
 const router = Router();
 
 router.post(
-  "/user/admin/signup",
+  "/user/admin/signup",auth,acessControl(typeUserEnum.ADMIN),
   userSignupValidatorMiddleware,
   adminController.createUserAdmin
 );
-router.post("/user/admin/login", adminController.loginUser);
+router.post("/user/admin/login",authMiddleware,acessControl(typeUserEnum.ADMIN), adminController.loginUser);
 
 module.exports = router;
