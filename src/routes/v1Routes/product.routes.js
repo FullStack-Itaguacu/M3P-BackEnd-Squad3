086248ffba276a01596ab945b878022121a99db2
fhoinaski <sukinodoncai@gmail.com>
2023-10-12 18:03:58 -0300

    fix: adicionado a validação na rota da requisição segura

--- src/routes/v1Routes/product.routes.js
+++ src/routes/v1Routes/product.routes.js
@@ -7,10 +7,10 @@ const checkProductOwner = require("../../middlewares/checkProductOwnerMiddleware
 const validatorBodyProduct = require("../../middlewares/validatorProductMiddleware");
 const router = Router();
 
-router.get("/products/admin",auth,acessControl(typeUserEnum.ADMIN),productController.getProducts);
+router.get("/products/admin/:offset/:limit",auth,acessControl(typeUserEnum.ADMIN),productController.getProducts);
 router.get("/products/:id", auth, productController.listProductId);
 router.get("/products/:offset/:limit", productController.getProducts);
 router.patch("/products/admin/:productId",auth,acessControl(typeUserEnum.ADMIN),checkProductOwner,productController.updateProduct);
-router.post("/products",auth,acessControl(typeUserEnum.ADMIN),validatorBodyProduct,productController.createProduct);
+router.post("/products/admin",auth,acessControl(typeUserEnum.ADMIN),validatorBodyProduct,productController.createProduct);
 
 module.exports = router;
