cc1db7e4aa4dd2262f0fc4cad576f76dcc7777b5
amarilis <amarilispequeno@gmail.com>
2023-10-11 23:11:02 -0300

    configura swagger - teste ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,12 @@ const userRoutes = require("./v1Routes/user.routes");
 const buyerRoutes = require("./v1Routes/buyer.routes");
 const saleRoutes = require("./v1Routes/sale.routes");
 const productRoutes = require("./v1Routes/product.routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("../swagger/swagger-output.json");
 
 const routes = new Router();
 
 routes.use("/api", [userRoutes, saleRoutes, productRoutes, buyerRoutes]);
+routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 module.exports = routes;
